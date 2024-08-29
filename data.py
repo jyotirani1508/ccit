1a7e75fad39f1871478b0a850aa6aa2a8bac4192
jyotirani1508 <jrani8581@gmail.com>
2024-08-29 10:01:29 +0530

    test2 commit

---
+++ data.py
@@ -0,0 +1,10 @@
+# importing required modules
+import matplotlib.pyplot as plt
+# values of x and y axes
+x = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50] 
+
+y = [1, 4, 3, 2, 7, 6, 9, 8, 10, 5]
+plt.plot(x, y)
+plt.xlabel('x')
+plt.ylabel('y') 
+plt.show() 
