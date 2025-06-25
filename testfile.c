d258d2fe0953ea303680bbffbac3e6bb1c6b456e
jyotirani1508 <jrani8581@gmail.com>
2025-06-25 18:29:02 +0530

    testfile

---
+++ testfile.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    //this program is for addition 
+   int a=100;
+   int h=20;
+   printf("a = %d",a);
+   printf("\nh = %d",h);
+   printf("\nsum of a and h = %d ",a+h);
+   
+}
